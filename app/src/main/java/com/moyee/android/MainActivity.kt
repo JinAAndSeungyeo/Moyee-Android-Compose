e8e1b61920509dbc2e062c4139a4926c53c5adb5
sseung416 <choiseungyeon416@gmail.com>
2023-02-07 15:13:51 +0900

    ➕ Add: NavigationItem 용 Screen sealed class 생성

--- app/src/main/java/com/moyee/android/MainActivity.kt
+++ app/src/main/java/com/moyee/android/MainActivity.kt
@@ -3,6 +3,12 @@ package com.moyee.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Chat
+import androidx.compose.material.icons.outlined.Map
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.QueueMusic
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.moyee.android.ui.theme.MoyeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,4 +20,11 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+sealed class Screen(val route: String, val icon: ImageVector) {
+    object Map : Screen("map", Icons.Outlined.Map)
+    object Chat : Screen("chat", Icons.Outlined.Chat)
+    object Playlist : Screen("playlist", Icons.Outlined.QueueMusic)
+    object User : Screen("user", Icons.Outlined.Person)
 }
