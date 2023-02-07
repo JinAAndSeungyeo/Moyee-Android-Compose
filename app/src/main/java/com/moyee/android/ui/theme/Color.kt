36b5df0df6d8d240707d9e7dbb6f57f200e6f002
sseung416 <choiseungyeon416@gmail.com>
2023-02-07 09:38:21 +0900

    ✨ Style: 앱 주요 색상 정의

--- app/src/main/java/com/moyee/android/ui/theme/Color.kt
+++ app/src/main/java/com/moyee/android/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.moyee.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Green500 = Color(0xFF31F4A2)
+val Purple800 = Color(0xFF332D41)
+val Purple900 = Color(0xFF1C1B1F)
+val Grey100 = Color(0xFFD9D9D9)
