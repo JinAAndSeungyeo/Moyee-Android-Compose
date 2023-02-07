d44926665a8077643b8f40fb368ad67b40f0c96c
sseung416 <choiseungyeon416@gmail.com>
2023-02-07 09:39:52 +0900

    ✨ Style: MoyeeTheme 설정

--- app/src/main/java/com/moyee/android/ui/theme/Theme.kt
+++ app/src/main/java/com/moyee/android/ui/theme/Theme.kt
@@ -1,42 +1,20 @@
 package com.moyee.android.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val GreenColors = darkColors(
+    primary = Green500,
+    surface = Purple800,
+    background = Purple900
 )
 
 @Composable
-fun MoyeeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun MoyeeTheme(content: @Composable () -> Unit) {
+    // default 가 다크 모드
     MaterialTheme(
-        colors = colors,
+        colors = GreenColors,
         typography = Typography,
         shapes = Shapes,
         content = content
