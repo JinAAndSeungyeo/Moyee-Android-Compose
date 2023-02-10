da5e6cfae5e162bd81fd1522df2f52b1e55db369
sseung416 <choiseungyeon416@gmail.com>
2023-02-10 13:42:37 +0900

    ➕ Add: UserLocation data class 추가

---
+++ app/src/main/java/com/moyee/android/domain/UserLocation.kt
@@ -0,0 +1,13 @@
+package com.moyee.android.domain
+
+import com.google.android.gms.maps.model.LatLng
+
+/**
+ * 내 위치 기반 근처 유저의 정보
+ * TODO: 서버단과 상의 후 데이터 클래스 변경 예정
+ * */
+data class UserLocation(
+    val id: Int,
+    val location: LatLng,
+    val imageUrl: String
+)
