83fdd60037a81650b7060e3be684bfc897eea4cf
sseung416 <choiseungyeon416@gmail.com>
2023-02-12 17:11:38 +0900

    ➕ Update: MoyeeColors color 값 수정 및 추가

--- app/src/main/java/com/moyee/android/ui/theme/Color.kt
+++ app/src/main/java/com/moyee/android/ui/theme/Color.kt
@@ -3,6 +3,8 @@ package com.moyee.android.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Green500 = Color(0xFF31F4A2)
+val Purple50 = Color(0xFFE8DEF8)
+val Purple400 = Color(0xFF814BFF)
+val Purple700 = Color(0xFF4A4458)
 val Purple800 = Color(0xFF332D41)
 val Purple900 = Color(0xFF1C1B1F)
-val Grey100 = Color(0xFFD9D9D9)
