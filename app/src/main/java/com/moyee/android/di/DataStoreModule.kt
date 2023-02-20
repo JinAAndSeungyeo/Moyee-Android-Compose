70c352c1dc36ea505b24c496a0d75fa851be17f2
sseung416 <choiseungyeon416@gmail.com>
2023-02-20 10:13:31 +0900

    ➕ Add: DataStoreModule Hilt 설정

---
+++ app/src/main/java/com/moyee/android/di/DataStoreModule.kt
@@ -0,0 +1,30 @@
+package com.moyee.android.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private const val SETTINGS_PREFERENCES = "setting_preferences"
+
+internal val FIRST_ACCESS_STATUS = booleanPreferencesKey("firstAccessStatus")
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataStoreModule {
+
+    @Singleton
+    @Provides
+    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            produceFile = { context.preferencesDataStoreFile(SETTINGS_PREFERENCES) }
+        )
+}
