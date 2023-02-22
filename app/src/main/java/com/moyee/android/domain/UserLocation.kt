248f02dbb1c03b3f4e1dd6dc5ffe0493de3e1ed6
sseung416 <choiseungyeon416@gmail.com>
2023-02-22 09:57:35 +0900

    🔨Refactor: UserLocation id 변수값 nullable로 변경

--- app/src/main/java/com/moyee/android/domain/UserLocation.kt
+++ app/src/main/java/com/moyee/android/domain/UserLocation.kt
@@ -7,7 +7,7 @@ import com.google.android.gms.maps.model.LatLng
  * TODO: 서버단과 상의 후 데이터 클래스 변경 예정
  * */
 data class UserLocation(
-    val id: Int,
+    val id: Int? = null,
     val location: LatLng,
     val imageUrl: String
 )
